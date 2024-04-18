5e8786d2b8ea6ae7fee6632e34e6d31d93c0e4ce
Manuel Manriquez <manriquezmanny@gmail.com>
2024-04-18 04:11:08 -0700

    Added state for getting form data on login and register pages

--- frontend/LeapList/src/Login.jsx
+++ frontend/LeapList/src/Login.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLogin((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
   return (
     <div id="register-root">
       <div className="register-container">
@@ -9,26 +27,22 @@ function Login() {
           className="login-item register-title"
           style={{ textAlign: "center" }}
         >
-          Log Into Account
+          Log In to Account
         </h1>
-        <form className="register-form">
+        <form className="register-form" onSubmit={handleSubmit}>
           <input
             type="email"
             name="email"
             placeholder="Email"
             className="login-item login-input"
-          ></input>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            className="login-item login-input"
+            onChange={handleChange}
           ></input>
           <input
             type="password"
             name="password"
             placeholder="Password"
             className="login-item login-input"
+            onChange={handleChange}
           ></input>
           <div className="login-item">
             <button name="Register" className="register-btn login-item">
--- frontend/LeapList/src/Register.jsx
+++ frontend/LeapList/src/Register.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Register() {
+  const [account, setAccount] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirm: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAccount((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
   return (
     <div id="register-root">
       <div className="register-container">
@@ -11,30 +31,34 @@ function Register() {
         >
           Make an Account
         </h1>
-        <form className="register-form">
+        <form className="register-form" onSubmit={handleSubmit}>
           <input
             type="email"
             name="email"
             placeholder="Email"
             className="login-item login-input"
+            onChange={handleChange}
           ></input>
           <input
             type="text"
             name="username"
             placeholder="Username"
             className="login-item login-input"
+            onChange={handleChange}
           ></input>
           <input
             type="password"
             name="password"
             placeholder="Password"
             className="login-item login-input"
+            onChange={handleChange}
           ></input>
           <input
             type="password"
             name="confirm"
             placeholder="Confirm Password"
             className="login-item login-input"
+            onChange={handleChange}
           ></input>
           <div className="login-item">
             <button name="Register" className="register-btn login-item">
